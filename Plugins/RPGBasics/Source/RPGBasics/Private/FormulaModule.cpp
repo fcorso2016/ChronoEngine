1d2a00ded327c781be79b74114fd5ef5abf51aa1
fcorso2016 <fcorso2016@gmail.com>
2019-09-18 16:56:38 -0400

    Removed cpp class

--- Plugins/RPGBasics/Source/RPGBasics/Private/FormulaModule.cpp
+++
@@ -1,9 +0,0 @@
-// Created by Francesco Corso. Labeled for reuse with attribution under Creative Commons.
-
-
-#include "FormulaModule.h"
-#include "Templates/SubclassOf.h"
-
-UFormulaModule* UFormulaModule::ConstructFormulaModule(UObject* Outer, TSubclassOf<UFormulaModule> ModuleClass) {
-	return NewObject<UFormulaModule>(Outer, ModuleClass);
-}
